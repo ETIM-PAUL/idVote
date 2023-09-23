0c39aea71481469aad047fb89d8498e699b85f9e
etim <etimpaul22@gmail.com>
2023-09-23 19:02:01 +0100

    added main app top nav

---
+++ frontend/pages/votingSystem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AppHeadNav } from '../components/AppNav'
+
+const votingSystem = () => {
+  return (
+    <div>
+      <AppHeadNav/>
+    </div>
+  )
+}
+
+export default votingSystem
